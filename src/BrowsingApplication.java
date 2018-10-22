1f972b634ab1921d1b309c7839ed547b4b020b3f
deepankur <deepankur.sadana@hansel.io>
2018-10-22 13:15:37 +0530

    adding initial classes

---
+++ src/BrowsingApplication.java
@@ -0,0 +1,9 @@
+
+public class BrowsingApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/models/Node.java
@@ -0,0 +1,7 @@
+package models;
+
+//This is the class which is presented to user and it has pointers to other nodes
+public class Node {
+	List<Node> suggestions 
+
+}
